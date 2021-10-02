e654c3b14d168f4261185947c35e943ff685a6fc
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-03 00:53:31 +0800

    Вывод категорий из базы на страницу категорий

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -24,7 +24,40 @@
 	<section class="content">
 		<div class="container-fluid">
 			<div class="row">
-
+				<div class="col-6">
+					<div class="card">
+						<div class="card-body table-responsive p-0">
+							<table class="table table-hover text-nowrap">
+								<thead>
+									<tr>
+										<th>ID</th>
+										<th>Название</th>
+										<th colspan="3" class="text-center">Деиствие</th>
+									</tr>
+								</thead>
+								<tbody>
+									@foreach($categories as $category)
+									<tr>
+										<td>{{$category->id}}</td>
+										<td>{{$category->title}}</td>
+										<td>
+											<a href=""><i class="far fa-eye"></i></a>
+										</td>
+										<td>
+											<a href="" class="text-success"><i class="fas fa-pencil-alt"></i></a>
+										</td>
+										<td>
+											<a href="" class="text-danger"><i class="fas fa-times"></i></a>
+										</td>
+									</tr>
+									@endforeach
+								</tbody>
+							</table>
+						</div>
+						<!-- /.card-body -->
+					</div>
+					<!-- /.card -->
+				</div>
 			</div>
 		</div>
 	</section>
