fa969a6cfea6e65db18a5f7740570494eb6d2dac
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-02 22:24:00 +0800

    Ссылка на странице категорий для добавления категории

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,37 +1,32 @@
 @extends('admin.layout.admin')
 @section('title', 'Админ панель-категории')
 @section('content')
-<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-	<!-- Content Header (Page header) -->
 	<div class="content-header">
 		<div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
 					<h1 class="m-0">Категории</h1>
-				</div><!-- /.col -->
+				</div>
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
 						<li class="breadcrumb-item"><a href="{{ route('admin') }}">Home</a></li>
 						<li class="breadcrumb-item active">Категории</li>
 					</ol>
-				</div><!-- /.col -->
-			</div><!-- /.row -->
-		</div><!-- /.container-fluid -->
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<a href="" class="btn btn-primary">Добавить</a>
+			</div>
+		</div>
 	</div>
-	<!-- /.content-header -->
 
-	<!-- Main content -->
 	<section class="content">
 		<div class="container-fluid">
-			<!-- Small boxes (Stat box) -->
 			<div class="row">
 
 			</div>
-			<!-- /.row -->
-		</div><!-- /.container-fluid -->
+		</div>
 	</section>
-	<!-- /.content -->
 </div>
-<!-- /.content-wrapper -->
 @endsection
