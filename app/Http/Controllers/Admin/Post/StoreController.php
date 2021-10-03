636f931daca482a18031bf7d6448fabafb61c5d8
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-03 16:36:27 +0800

    StoreControler - добавление изображений в базу

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Admin\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
 	public function __invoke(StoreRequest $request)
 	{
+
 		$data = $request->validated();
+		$data['preview_img'] = Storage::put('/images', $data['preview_img']);
+		$data['main_img'] = Storage::put('/images', $data['main_img']);
 		Post::firstOrCreate($data);
 
 		return redirect()->route('admin.posts');
