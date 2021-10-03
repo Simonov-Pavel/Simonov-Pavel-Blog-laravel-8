da40a607d533714f02a5d1f38c36b304faf2a0cb
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-03 19:16:51 +0800

    Транзакция

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -5,20 +5,26 @@ namespace App\Http\Controllers\Admin\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Models\Post;
+use Exception;
 use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
 	public function __invoke(StoreRequest $request)
 	{
-		$data = $request->validated();
-		$tagIds = $data['tag_ids'];
-		unset($data['tag_ids']);
+		try {
+			$data = $request->validated();
+			$tagIds = $data['tag_ids'];
+			unset($data['tag_ids']);
 
-		$data['preview_img'] = Storage::put('/images', $data['preview_img']);
-		$data['main_img'] = Storage::put('/images', $data['main_img']);
-		$post = Post::firstOrCreate($data);
-		$post->tags()->attach($tagIds);
+			$data['preview_img'] = Storage::put('/images', $data['preview_img']);
+			$data['main_img'] = Storage::put('/images', $data['main_img']);
+			$post = Post::firstOrCreate($data);
+			$post->tags()->attach($tagIds);
+		} catch (Exception $exception) {
+			abort(404);
+		}
+
 
 		return redirect()->route('admin.posts');
 	}
