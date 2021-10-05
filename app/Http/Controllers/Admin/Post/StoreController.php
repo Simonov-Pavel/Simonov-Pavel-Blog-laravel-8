e0ac1c4ce4c6ec18693d02da26cafe37347e2e10
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-05 19:08:13 +0800

    Указываем конкретную папку для изображений

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -17,8 +17,8 @@ class StoreController extends Controller
 			$tagIds = $data['tag_ids'];
 			unset($data['tag_ids']);
 
-			$data['preview_img'] = Storage::put('/images', $data['preview_img']);
-			$data['main_img'] = Storage::put('/images', $data['main_img']);
+			$data['preview_img'] = Storage::disk('public')->put('/images', $data['preview_img']);
+			$data['main_img'] = Storage::disk('public')->put('/images', $data['main_img']);
 			$post = Post::firstOrCreate($data);
 			$post->tags()->attach($tagIds);
 		} catch (Exception $exception) {
