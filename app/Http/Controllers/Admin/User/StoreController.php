f5c7daee152c38c4848a71537509951aeafa7383
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-06 15:36:02 +0800

    StoreController User

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -1,19 +1,18 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Category;
+namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Category\StoreRequest;
-use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\User\StoreRequest;
+use App\Models\User;
 
 class StoreController extends Controller
 {
 	public function __invoke(StoreRequest $request)
 	{
 		$data = $request->validated();
-		Category::firstOrCreate($data);
+		User::firstOrCreate($data);
 
-		return redirect()->route('admin.categories');
+		return redirect()->route('admin.user');
 	}
 }
