811201c45cdac75fc69fb95b64089e610d932839
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-06 15:55:21 +0800

    edit.blade

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,17 +1,17 @@
 @extends('admin.layout.admin')
-@section('title')Редактирование {{$category->title}}@endsection
+@section('title')Редактирование {{$user->name}}@endsection
 @section('content')
 <div class="content-wrapper">
 	<div class="content-header">
 		<div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
-					<h1 class="m-0">Редактирование {{$category->title}}</h1>
+					<h1 class="m-0">Редактирование {{$user->name}}</h1>
 				</div>
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
 						<li class="breadcrumb-item"><a href="{{ route('admin') }}">Home</a></li>
-						<li class="breadcrumb-item"><a href="{{ route('admin.categories') }}">Категории</a></li>
+						<li class="breadcrumb-item"><a href="{{ route('admin.categories') }}">Пользователи</a></li>
 						<li class="breadcrumb-item active">Редактировать</li>
 					</ol>
 				</div>
@@ -22,12 +22,12 @@
 	<section class="content">
 		<div class="container-fluid">
 			<div class="col-6">
-				<form action="{{ route('admin.categories.update', $category->id) }}" method="post">
+				<form action="{{ route('admin.users.update', $user->id) }}" method="post">
 					@csrf
 					@method('PATCH')
 					<div class="form-group">
 						<label>Название категории</label>
-						<input type="text" class="form-control" name="title" placeholder="Введите название категории" value="{{$category->title}}" required>
+						<input type="text" class="form-control" name="name" placeholder="Введите имя пользователя" value="{{$user->name}}" required>
 
 						@error('title')
 						<div class="text-danger">Это поле обязательно для заполнения</div>
