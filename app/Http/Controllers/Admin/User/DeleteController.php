848db1cd92178860783d2c2cd60b1ac9e1f0fa9f
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-06 15:26:53 +0800

    DeleteController User2

--- app/Http/Controllers/Admin/User/DeleteController.php
+++ app/Http/Controllers/Admin/User/DeleteController.php
@@ -1,18 +1,16 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Category;
+namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Category\UpdateRequest;
-use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class DeleteController extends Controller
 {
-	public function __invoke(Category $category)
+	public function __invoke(User $user)
 	{
-		$category->delete();
+		$user->delete();
 
-		return redirect()->route('admin.categories');
+		return redirect()->route('admin.users');
 	}
 }
