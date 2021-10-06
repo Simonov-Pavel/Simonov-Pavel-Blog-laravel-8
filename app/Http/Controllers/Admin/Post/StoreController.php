42ec68338f35ca74f5eb31157627b1d73ccdee9d
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-06 13:28:53 +0800

    StoreController

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -2,28 +2,17 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Models\Post;
 use Exception;
 use Illuminate\Support\Facades\Storage;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
 	public function __invoke(StoreRequest $request)
 	{
-		try {
-			$data = $request->validated();
-			$tagIds = $data['tag_ids'];
-			unset($data['tag_ids']);
-
-			$data['preview_img'] = Storage::disk('public')->put('/images', $data['preview_img']);
-			$data['main_img'] = Storage::disk('public')->put('/images', $data['main_img']);
-			$post = Post::firstOrCreate($data);
-			$post->tags()->attach($tagIds);
-		} catch (Exception $exception) {
-			abort(404);
-		}
+		$data = $request->validated();
+		$this->service->store($data);
 		return redirect()->route('admin.posts');
 	}
 }
