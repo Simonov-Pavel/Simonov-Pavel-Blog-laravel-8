0597439d0311357f6e15bac54cf6f34a316799bc
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-06 15:57:42 +0800

    create.blade

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -1,17 +1,17 @@
 @extends('admin.layout.admin')
-@section('title', 'Админ панель-добавление категории')
+@section('title', 'Админ панель-добавление пользователя')
 @section('content')
 <div class="content-wrapper">
 	<div class="content-header">
 		<div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
-					<h1 class="m-0">Добавление категории</h1>
+					<h1 class="m-0">Добавление пользователя</h1>
 				</div>
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
 						<li class="breadcrumb-item"><a href="{{ route('admin') }}">Home</a></li>
-						<li class="breadcrumb-item"><a href="{{ route('admin.categories') }}">Категории</a></li>
+						<li class="breadcrumb-item"><a href="{{ route('admin.users') }}">Пользователи</a></li>
 						<li class="breadcrumb-item active">Добавление</li>
 					</ol>
 				</div>
@@ -22,11 +22,11 @@
 	<section class="content">
 		<div class="container-fluid">
 			<div class="col-6">
-				<form action="{{ route('admin.categories.store') }}" method="post">
+				<form action="{{ route('admin.users.store') }}" method="post">
 					@csrf
 					<div class="form-group">
-						<label>Название категории</label>
-						<input type="text" class="form-control" name="title" placeholder="Введите название категории" required>
+						<label>Имя</label>
+						<input type="text" class="form-control" name="title" placeholder="Введите имя пользователя" required>
 						@error('title')
 						<div class="text-danger">{{$message}}</div>
 						@enderror
