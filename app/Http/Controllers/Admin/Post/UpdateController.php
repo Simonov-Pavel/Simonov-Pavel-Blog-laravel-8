74f39dadad9b28643a484d0c19956940ec00c903
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-06 13:38:03 +0800

    PostService function update, UpdateController

--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -12,17 +12,7 @@ class UpdateController extends Controller
 	public function __invoke(UpdateRequest $request, Post $post)
 	{
 		$data = $request->validated();
-		$tagIds = $data['tag_ids'];
-		unset($data['tag_ids']);
-		if (isset($data['preview_img'])) {
-			$data['preview_img'] = Storage::disk('public')->put('/images', $data['preview_img']);
-		}
-		if (isset($data['main_img'])) {
-			$data['main_img'] = Storage::disk('public')->put('/images', $data['main_img']);
-		}
-		$post->update($data);
-		$post->tags()->sync($tagIds);
-		$posts = Post::all();
-		return view('admin.post.index', compact('posts'));
+		$post = $this->service->update($data, $post);
+		return view('admin.post.show', compact('post'));
 	}
 }
--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -14,11 +14,33 @@ class PostService
 
 			$tagIds = $data['tag_ids'];
 			unset($data['tag_ids']);
-
-			$data['preview_img'] = Storage::disk('public')->put('/images', $data['preview_img']);
-			$data['main_img'] = Storage::disk('public')->put('/images', $data['main_img']);
+			if (isset($data['preview_img'])) {
+				$data['preview_img'] = Storage::disk('public')->put('/images', $data['preview_img']);
+			}
+			if (isset($data['main_img'])) {
+				$data['main_img'] = Storage::disk('public')->put('/images', $data['main_img']);
+			}
 			$post = Post::firstOrCreate($data);
 			$post->tags()->attach($tagIds);
+		} catch (Exception $exception) {
+			abort(404);
+		}
+	}
+
+	public function update($data, $post)
+	{
+		try {
+
+			$tagIds = $data['tag_ids'];
+			unset($data['tag_ids']);
+			if (isset($data['preview_img'])) {
+				$data['preview_img'] = Storage::disk('public')->put('/images', $data['preview_img']);
+			}
+			if (isset($data['main_img'])) {
+				$data['main_img'] = Storage::disk('public')->put('/images', $data['main_img']);
+			}
+			$post->update($data);
+			$post->tags()->sync($tagIds);
 		} catch (Exception $exception) {
 			abort(404);
 		}
