28f9cb2715cfde9034ac97ca5e03c2f62fa86e08
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-09 02:05:08 +0800

    В админку впускаем только admin

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        return $next($request);
-    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle(Request $request, Closure $next)
+	{
+		if ((int)auth()->user()->role !== User::ROLE_ADMIN) {
+			abort(404);
+		}
+		return $next($request);
+	}
 }
