19ed324e0253605ab2290174b7193b948306a47a
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-09 16:09:20 +0800

    Переносим реализацию с StoreController в StoreUserJob

--- app/Jobs/StoreUserJob.php
+++ app/Jobs/StoreUserJob.php
@@ -8,28 +8,39 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use App\Mail\User\PasswordMail;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class StoreUserJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+	/**
+	 * Create a new job instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle()
-    {
-        //
-    }
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		$password = Str::random(10);
+		$data['password'] = Hash::make($password);
+		$user = User::firstOrCreate(['email' => $data['email']], $data);
+
+		Mail::to($data['email'])->send(new PasswordMail($password));
+		event(new Registered($user));
+	}
 }
