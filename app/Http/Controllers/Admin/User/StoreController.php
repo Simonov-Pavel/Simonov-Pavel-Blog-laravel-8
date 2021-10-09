04d923db61f586f70cf2689e435ee3e2e9dcf61c
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-09 16:17:28 +0800

    StoreUserJob::dispatch();

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
-
+use App\Jobs\StoreUserJob;
 
 class StoreController extends Controller
 {
 	public function __invoke(StoreRequest $request)
 	{
 		$data = $request->validated();
+
+		StoreUserJob::dispatch($data);
 
 		return redirect()->route('admin.users');
 	}
