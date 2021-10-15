cae738f467cf95697a585327fc386f08e9180c52
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2021-10-15 19:09:00 +0800

    Controller User/IndexControler

---
+++ app/Http/Controllers/Account/User/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Account\User;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+	public function __invoke()
+	{
+		$user = auth()->user();
+		return view('account.main.index', compact('user'));
+	}
+}
